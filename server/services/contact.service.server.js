178516d703adcea233211e4910502f7365de24ed
TSN Student <meriem.saaid@gmail.com>
2019-01-25 15:57:16 -0500

    fixing  known issues with mail gun

--- server/services/contact.service.server.js
+++ server/services/contact.service.server.js
@@ -3,23 +3,19 @@ module.exports = function(app) {
   app.post("/api/contact", sendMessage);
 
   async function sendMessage(req, res) {
-    var API_KEY = "";
-    var DOMAIN = "";
-    if (process.env.MLAB_USERNAME_WEBDEV) {
-      API_KEY = process.env.MAILGUN_API_KEY;
-      DOMAIN = process.env.MAILGUN_DOMAIN;
-    }
-    var mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
-
-    const data = {
-      from: req.body.email,
-      to: "msimbo2018@gmail.com",
-      subject: req.body.subject,
-      text: req.body.message
-    };
-
-    mailgun.messages().send(data, (error, body) => {
-      console.log(body);
-    });
+    API_KEY = process.env.MAILGUN_API_KEY;
+    DOMAIN = process.env.MAILGUN_DOMAIN;
   }
+  var mailgun = require("mailgun-js")({ apiKey: API_KEY, domain: DOMAIN });
+
+  const data = {
+    from: req.body.email,
+    to: "msimbo2018@gmail.com",
+    subject: req.body.subject,
+    text: req.body.message
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+  });
 };
