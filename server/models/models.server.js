71cb005c019b75619d85802b804a4f1b33b494cb
MeriemSaaid <meriem.saaid@gmail.com>
2019-01-28 03:06:00 -0500

    Final touch server side

--- server/models/models.server.js
+++ server/models/models.server.js
@@ -4,14 +4,26 @@
 // check if running remotely
 
 // var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
-var username = "admin";
-//var password = process.env.MLAB_PASSWORD_WEBDEV;
-var password = "admin123";
+// var username = "admin";
+// //var password = process.env.MLAB_PASSWORD_WEBDEV;
+// var password = "admin123";
 
-connectionString = "mongodb://admin:admin123";
+// connectionString = "mongodb://admin:admin123";
 
-connectionString += "@ds115045.mlab.com:15045/food_lover"; // use yours
-// }
+// connectionString += "@ds115045.mlab.com:15045/food_lover"; // use yours
+// // }
+
+if (process.env.MLAB_USERNAME_WEBDEV) {
+  // check if running remotely
+
+  var username = process.env.MLAB_USERNAME_WEBDEV; // get from environment
+
+  var password = process.env.MLAB_PASSWORD_WEBDEV;
+
+  connectionString = "mongodb://" + username + ":" + password;
+
+  connectionString += "@ds115045.mlab.com:15045/food_lover"; // use yours
+}
 
 var mongoose = require("mongoose");
 
