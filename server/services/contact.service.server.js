656a4bbe0640723b6517acaeab95e8d312cc552c
wilson jean-louis <wilson.jeanlouis@yahoo.com>
2019-06-02 16:59:41 -0400

    updating some dependecnies

--- server/services/contact.service.server.js
+++ server/services/contact.service.server.js
@@ -1,9 +1,14 @@
 module.exports = function(app) {
     const UserModel = require("../models/user/user.model.server");
-    
+    app.post('/api/contact', sendMessage)
+
+
+    async function sendMessage(req, res) {
     var API_KEY = 'YOUR_API_KEY';
 var DOMAIN = 'YOUR_DOMAIN_NAME';
 var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
+
+      const data = req.body;
 
 const data = {
   from: 'Excited User <me@samples.mailgun.org>',
@@ -15,4 +20,5 @@ const data = {
 mailgun.messages().send(data, (error, body) => {
   console.log(body);
 });
+}
 }
